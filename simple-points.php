d5456e4d6c17de5ae79b47a7504288320ba239c0
Ryan Imel <ryan@aspiringindie.com>
2012-09-22 00:50:22 -0400

    Initial plugin file.

---
+++ simple-points.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+Plugin Name: Simple Points
+Plugin URI: TBD
+Description: Award points to your users.
+Version: 0.1.alpha-201209
+Author: Ryan Imel
+Author URI: http://wpcandy.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
